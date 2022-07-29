b687a501c4e78a65c61f1b0ab77aaff6542c2ac3
dlukin <dlukin@phoenixit.ru>
2022-07-29 17:16:41 +0300

    init commit

---
+++ src/main/java/phoenixit/education/EducationModelApiApplication.java
@@ -0,0 +1,15 @@
+package phoenixit.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Launching the EducationModelApi application.
+ */
+@SpringBootApplication
+public class EducationModelApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EducationModelApiApplication.class, args);
+  }
+}
